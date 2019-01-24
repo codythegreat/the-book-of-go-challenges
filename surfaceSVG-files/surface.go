0dded0acc205f47ab7ef43398c9f898b7c61dc3e
EC2 Default User <maxiecody@gmail.com>
2019-01-24 19:06:34 +0000

    added surface.go and mandelbrot.go

---
+++ surfaceSVG-files/surface.go
@@ -0,0 +1,55 @@
+// computes an SVG rendering of a 3-d surface function.
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	width, height = 600, 320            // canvas size in px
+	cells         = 100                 // number of grid cells
+	xyrange       = 30.0                // axis ranges(-xy range through +xy range)
+	xyscale       = width / 2 / xyrange // px per x or y unit
+	zscale        = height * 0.4        // px per z unit
+	angle         = math.Pi / 6         // angle of x, y axes (=30 degrees)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30degrees), cos(30degrees)
+
+func main() {
+	fmt.Printf("<svg xmlns='https://www.w3.org/2000/svg' "+
+		"style='stronke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+1, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1, j+1)
+			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+	fmt.Println("</svg>")
+}
+
+func corner(i, j int) (float64, float64) {
+	// Find point (x,y) at corner of cell (i, j).
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	// compute surface height Z
+
+	z := f(x, y)
+
+	// project (x, y, z) isometrically onto 2-D svg canvas (sx, sy)
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y) // dist from (0, 0)
+	return math.Sin(r) / r
+}
