6282dbe5c74ea89c4c0da326f13d9657f2ef8b22
codythegreat <maxiecody@gmail.com>
2019-02-06 15:32:08 +0000

    added outline2 file to repo (passing functions)

---
+++ findlinks-files/outline2.go
@@ -0,0 +1,47 @@
+// forEachNode calls teh functions pre(x) and post(x) for each node
+// x in the tree rooted at n. Both functions are optional.
+// pre is called before the children are visited (preorder) and
+// post is called after (postorder).
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"strings"
+)
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
+
+func main() {
+	htmlString := `<h1>Hello World!</h1><p>My name is Cody</p>`
+	doc, err := html.Parse(strings.NewReader(htmlString))
+	if err != nil {
+		fmt.Printf("tried parsing htmlString: %v", err)
+	}
+	var depth int
+	pre := func(node *html.Node) {
+		if node.Type == html.ElementNode {
+			fmt.Printf("%*s<%s>\n", depth*2, "", node.Data)
+			depth++
+		}
+	}
+	post := func(node *html.Node) {
+		if node.Type == html.ElementNode {
+			depth--
+			fmt.Printf("%*s</%s>\n", depth*2, "", node.Data)
+		}
+	}
+	forEachNode(doc, pre, post)
+}
